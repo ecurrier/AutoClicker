8183f0c6e4b264b64fe4fd11ebab0418c7a593d5
ecurrier <ecurri3@gmail.com>
2017-07-29 17:27:21 -0500

    Initial Commit

---
+++ AutoClicker/AutoClicker/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace AutoClicker
+{
+    partial class autoClickerForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.startButton = new System.Windows.Forms.Button();
+            this.endButton = new System.Windows.Forms.Button();
+            this.replayButton = new System.Windows.Forms.Button();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.timerLabel = new System.Windows.Forms.Label();
+            this.timerText = new System.Windows.Forms.TextBox();
+            this.clickLog = new System.Windows.Forms.ListView();
+            this.xCoordinate = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.yCoordinate = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.timeOccurred = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.stopRecordingButton = new System.Windows.Forms.Button();
+            this.resetButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(12, 12);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(201, 55);
+            this.startButton.TabIndex = 0;
+            this.startButton.Text = "Start Recording";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // endButton
+            // 
+            this.endButton.Location = new System.Drawing.Point(219, 12);
+            this.endButton.Name = "endButton";
+            this.endButton.Size = new System.Drawing.Size(189, 55);
+            this.endButton.TabIndex = 1;
+            this.endButton.Text = "End Recording";
+            this.endButton.UseVisualStyleBackColor = true;
+            this.endButton.Click += new System.EventHandler(this.endButton_Click);
+            // 
+            // replayButton
+            // 
+            this.replayButton.Location = new System.Drawing.Point(12, 417);
+            this.replayButton.Name = "replayButton";
+            this.replayButton.Size = new System.Drawing.Size(104, 47);
+            this.replayButton.TabIndex = 3;
+            this.replayButton.Text = "Replay Recording";
+            this.replayButton.UseVisualStyleBackColor = true;
+            this.replayButton.Click += new System.EventHandler(this.replayButton_Click);
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(237, 417);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(171, 47);
+            this.progressBar.TabIndex = 5;
+            // 
+            // timerLabel
+            // 
+            this.timerLabel.AutoSize = true;
+            this.timerLabel.Location = new System.Drawing.Point(14, 77);
+            this.timerLabel.Name = "timerLabel";
+            this.timerLabel.Size = new System.Drawing.Size(33, 13);
+            this.timerLabel.TabIndex = 6;
+            this.timerLabel.Text = "Time:";
+            // 
+            // timerText
+            // 
+            this.timerText.Location = new System.Drawing.Point(53, 74);
+            this.timerText.Name = "timerText";
+            this.timerText.Size = new System.Drawing.Size(100, 20);
+            this.timerText.TabIndex = 7;
+            // 
+            // clickLog
+            // 
+            this.clickLog.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.xCoordinate,
+            this.yCoordinate,
+            this.timeOccurred});
+            this.clickLog.Location = new System.Drawing.Point(12, 105);
+            this.clickLog.Name = "clickLog";
+            this.clickLog.Size = new System.Drawing.Size(396, 295);
+            this.clickLog.TabIndex = 8;
+            this.clickLog.UseCompatibleStateImageBehavior = false;
+            this.clickLog.View = System.Windows.Forms.View.Details;
+            // 
+            // xCoordinate
+            // 
+            this.xCoordinate.Text = "X";
+            this.xCoordinate.Width = 88;
+            // 
+            // yCoordinate
+            // 
+            this.yCoordinate.Text = "Y";
+            this.yCoordinate.Width = 87;
+            // 
+            // timeOccurred
+            // 
+            this.timeOccurred.Text = "Time";
+            this.timeOccurred.Width = 245;
+            // 
+            // stopRecordingButton
+            // 
+            this.stopRecordingButton.Location = new System.Drawing.Point(123, 417);
+            this.stopRecordingButton.Name = "stopRecordingButton";
+            this.stopRecordingButton.Size = new System.Drawing.Size(108, 47);
+            this.stopRecordingButton.TabIndex = 9;
+            this.stopRecordingButton.Text = "Stop Recording";
+            this.stopRecordingButton.UseVisualStyleBackColor = true;
+            this.stopRecordingButton.Click += new System.EventHandler(this.stopRecordingButton_Click);
+            // 
+            // resetButton
+            // 
+            this.resetButton.Location = new System.Drawing.Point(259, 73);
+            this.resetButton.Name = "resetButton";
+            this.resetButton.Size = new System.Drawing.Size(110, 24);
+            this.resetButton.TabIndex = 10;
+            this.resetButton.Text = "Reset";
+            this.resetButton.UseVisualStyleBackColor = true;
+            this.resetButton.Click += new System.EventHandler(this.resetButton_Click);
+            // 
+            // autoClickerForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(421, 479);
+            this.Controls.Add(this.resetButton);
+            this.Controls.Add(this.stopRecordingButton);
+            this.Controls.Add(this.clickLog);
+            this.Controls.Add(this.timerText);
+            this.Controls.Add(this.timerLabel);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.replayButton);
+            this.Controls.Add(this.endButton);
+            this.Controls.Add(this.startButton);
+            this.Name = "autoClickerForm";
+            this.Text = "Auto Clicker";
+            this.TopMost = true;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Button endButton;
+        private System.Windows.Forms.Button replayButton;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Label timerLabel;
+        private System.Windows.Forms.TextBox timerText;
+        private System.Windows.Forms.ListView clickLog;
+        private System.Windows.Forms.ColumnHeader xCoordinate;
+        private System.Windows.Forms.ColumnHeader yCoordinate;
+        private System.Windows.Forms.ColumnHeader timeOccurred;
+        private System.Windows.Forms.Button stopRecordingButton;
+        private System.Windows.Forms.Button resetButton;
+    }
+}
+
---
+++ AutoClicker/AutoClicker/Form1.cs
@@ -0,0 +1,156 @@
+﻿using Gma.UserActivityMonitor;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace AutoClicker
+{
+    public partial class autoClickerForm : Form
+    {
+        [DllImport("user32.dll")]
+        static extern void mouse_event(int dwFlags, int dx, int dy, int dwData, int dwExtraInfo);
+
+        private const int MOUSEEVENTF_MOVE = 0x0001;
+        private const int MOUSEEVENTF_LEFTDOWN = 0x0002;
+        private const int MOUSEEVENTF_LEFTUP = 0x0004;
+
+        System.Windows.Forms.Timer timer = new System.Windows.Forms.Timer();
+        Stopwatch stopwatch = new Stopwatch();
+        List<int> clickXCoordinates = new List<int>();
+        List<int> clickYCoordinates = new List<int>();
+        List<long> clickTimes = new List<long>();
+        
+        BackgroundWorker backgroundWorker = new BackgroundWorker();
+
+        public autoClickerForm()
+        {
+            InitializeComponent();
+
+            backgroundWorker.DoWork += backgroundworker_DoWork;
+            backgroundWorker.ProgressChanged += backgroundworker_ProgressChanged;
+            backgroundWorker.WorkerReportsProgress = true;
+            backgroundWorker.WorkerSupportsCancellation = true;
+        }
+
+        private void HookManager_MouseDown(object sender, MouseEventArgs e)
+        {
+            clickXCoordinates.Add(e.X);
+            clickYCoordinates.Add(e.Y);
+            clickTimes.Add(stopwatch.ElapsedMilliseconds);
+
+            var clickLogItem = new ListViewItem(e.X.ToString())
+            {
+                SubItems =
+                {
+                    e.Y.ToString(),
+                    $"{stopwatch.Elapsed.Minutes}:{stopwatch.Elapsed.Seconds}:{stopwatch.Elapsed.Milliseconds}"
+                }
+            };
+
+            clickLog.Items.Add(clickLogItem);
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            timer.Interval = 10;
+            timer.Tick += new EventHandler(timerTick);
+            timer.Start();
+            
+            stopwatch.Start();
+
+            // Hook MouseDown function
+            HookManager.MouseDown += HookManager_MouseDown;
+        }
+
+        private void endButton_Click(object sender, EventArgs e)
+        {
+            // Unhook MouseDown function
+            HookManager.MouseDown -= HookManager_MouseDown;
+
+            // Remove the click recorded for ending the recording
+            clickXCoordinates.RemoveAt(clickXCoordinates.Count - 1);
+            clickYCoordinates.RemoveAt(clickYCoordinates.Count - 1);
+            clickTimes.RemoveAt(clickTimes.Count - 1);
+            clickLog.Items.RemoveAt(clickLog.Items.Count - 1);
+
+            timer.Stop();
+
+            stopwatch.Reset();
+            stopwatch.Stop();
+        }
+
+        private void timerTick(object sender, EventArgs e)
+        {
+            var elapsed = stopwatch.Elapsed;
+            timerText.Text = $"{elapsed.Minutes}:{elapsed.Seconds}:{elapsed.Milliseconds}";
+        }
+
+        public void MouseClick(int x, int y)
+        {
+            Cursor.Position = new Point(x, y);
+
+            Thread.Sleep(250);
+
+            mouse_event(MOUSEEVENTF_LEFTDOWN, Cursor.Position.X, Cursor.Position.Y, 0, 0);
+            mouse_event(MOUSEEVENTF_LEFTUP, Cursor.Position.X, Cursor.Position.Y, 0, 0);
+        }
+
+        private void replayButton_Click(object sender, EventArgs e)
+        {
+            progressBar.Maximum = clickTimes.Count;
+
+            backgroundWorker.RunWorkerAsync();
+        }
+
+        private void backgroundworker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            var playbackStopWatch = new Stopwatch();
+            playbackStopWatch.Start();
+
+            while (true)
+            {
+                int i = 0;
+                while (i < clickTimes.Count)
+                {
+                    if (backgroundWorker.CancellationPending)
+                    {
+                        e.Cancel = true;
+                        return;
+                    }
+
+                    if (clickTimes[i] <= playbackStopWatch.ElapsedMilliseconds)
+                    {
+                        playbackStopWatch.Stop();
+                        MouseClick(clickXCoordinates[i], clickYCoordinates[i]);
+                        i++;
+                        backgroundWorker.ReportProgress(i);
+                        playbackStopWatch.Start();
+                    }
+                }
+            }
+        }
+
+        private void backgroundworker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            progressBar.Value = e.ProgressPercentage;
+        }
+
+        private void resetButton_Click(object sender, EventArgs e)
+        {
+            clickXCoordinates.Clear();
+            clickYCoordinates.Clear();
+            clickTimes.Clear();
+            clickLog.Items.Clear();
+        }
+
+        private void stopRecordingButton_Click(object sender, EventArgs e)
+        {
+            backgroundWorker.CancelAsync();
+        }
+    }
+}
